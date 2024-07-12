d4ea2ffd5798101765695e3744455c4e21a1f21a
abdumomin-ai <155010267+abdumomin-ai@users.noreply.github.com>
2024-07-12 11:15:40 +0500

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -10,7 +10,7 @@ public class Triangle extends GeometricFigure {
 
     public static void main(String[] args) {
         Triangle triangle = new Triangle();
-        System.out.println(triangle.TrapesiumArea(3,4,4));
+        System.out.println(triangle.TriangleArea(3,4,4));
         System.out.println(triangle.TrianglePerimeter(4,6,7));
     }
 }
